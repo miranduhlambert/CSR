5ffe5b6a0e1e2ef938fc6d9bc1bea52a68e224e2
zynpltrk <zuluturk@utexas.edu>
2024-05-22 11:08:49 -0500

    Try commit againnnn

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,5 @@
 print("Hello World! :)")
 print("Test print")
-<<<<<<< HEAD
-print("Testing add")
-=======
 print("Testing add")
 print(":P")
->>>>>>> 1047339cfa9e86ff84700f3293ed416e4f14631e
+print("ADD")
