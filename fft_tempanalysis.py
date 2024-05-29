0ac3b81a842751b1a04c97f5ebb718ac20a59666
miranduhlambert <mrlambert@utexas.edu>
2024-05-29 14:44:43 -0500

    New document for the future fast fourier transform analysis

---
+++ fft_tempanalysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#Importing data to read
+mydata = pd.read_table("C:\\data\\gracefo_1A_2020-07-01_RL04.ascii.noLRI.tgz\\ACC1A_2020-07-01_C_04.txt")
+# file path join to read from separate space
+
+#Filtering Data Product to exclusively get temperature data
