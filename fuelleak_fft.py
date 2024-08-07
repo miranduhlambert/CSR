fb8029b7c97f800b50a212b994355207c7c2ba13
miranduhlambert <mrlambert@utexas.edu>
2024-08-07 13:41:11 -0500

    Almost Debugged Fuel Leak FFT Code, SCORE!

--- fuelleak_fft.py
+++ fuelleak_fft.py
@@ -15,8 +15,7 @@ def check_product_flag(product_flag):
     # Define the sequences you are looking for
     sequences = {
         'Temperatures': '01110100', #This and the following sequence for Van-Der-Waal data extractions
-        'Pressures': '00000011',
-        'Fuel Mass Estimates': '11000000' #Fuel Estimate Files
+        'Pressures': '00000011'
         }
     
     #Using the key in the defined sequences, if the porduct flag is in the key return key 
@@ -24,82 +23,97 @@ def check_product_flag(product_flag):
         if product_flag in sequences[key]:
             # If you find a match, exit the loop and return True
             return key
-   
     #if no match is found, return None
     return None
 
 
 #Function for filtering data to Bypass Yaml Header marker
-def process_file_past_header(filename, marker, product_flag_index, data_indices, time_s_index, time_ms_index, data_vectors, time_vector):
+def process_file_past_header(filename, marker, product_flag_index, data_indices, tank_num_index, time_s_index, time_ms_index, data_vectors, time_vector):
     #Define Variables for Lop Iteration
     marker_found= False
     reference_time_seconds=None
 
+    # Initialize pressure storage
+    last_pressure_values = {'1': None, '2': None}  # Assuming tanks are numbered 1 and 2
+
     with open(filename,'r') as file:
-
         for line in file:
+            line=line.strip()
+            if marker_found== True: # snippet to get all the data part the marker, do not have to set marker_found == True because it is defined with  
+                columns = line.split() #Ensure the line is being treated as a string
+                product_flag = columns[product_flag_index]
+                product_flag_key = check_product_flag(product_flag)
 
-            if marker_found: # snippet to get all the data part the marker, do not have to set marker_found == True because it is defined with  
-        
-                columns = line.split()
-
-              # Ensure the line has at least 5 columns to avoid index error
-                if len(columns) > max(product_flag_index, *data_indices, time_s_index, time_ms_index):
-                    
-                    product_flag = columns[product_flag_index]
-
-                    product_flag_key = check_product_flag(product_flag)
-                    
-                    if product_flag_key== 'Fuel Mass Estimates':
-                        fuel_mass_est_A = float(columns[data_indices[0]])
-                        fuel_mass_est_B = float(columns[data_indices[1]])
-                        data_vectors['fuel_est'].append
+                if product_flag_key:
+                    tank_num = str(int(columns[tank_num_index]))
                         
-
-                    if product_flag_key:
-                        reg_press = float(columns[data_indices[0]])
-                        skin_temp=float(columns[data_indices[1]])
-                        skin_temp_r=float(columns[data_indices[2]])
-                        boss_fixed=float(columns[data_indices[3]])
-
+                    if product_flag_key == 'Pressures':  # Check if this is a pressure line
+                        # Update last seen pressure values
+                        last_pressure_values[tank_num] = {
+                            'tank_press': float(columns[data_indices[0]]),
+                            'reg_press': float(columns[data_indices[1]])
+                        }
                         if reference_time_seconds is None:
-                            reference_time_seconds = float(columns[time_s_index])
-                    
-                        time_ms = float(columns[time_ms_index]) / 1000000  # Convert microseconds to seconds
+                                reference_time_seconds = float(columns[time_s_index])
+                        time_ms = float(columns[time_ms_index]) / 1000000
                         time_seconds = float(columns[time_s_index]) - reference_time_seconds
-                        time_combined= time_seconds + time_ms  # Combine seconds and milliseconds
+                        time_combined = time_seconds + time_ms
 
-                        #Append data    
-                        data_vectors['reg_press'].append(float(reg_press))
-                        data_vectors['skin_temp'].append(float(skin_temp))
-                        data_vectors['skin_temp_r'].append(float(skin_temp_r))
-                        data_vectors['boss_fixed'].append(float(boss_fixed))
-                        time_vector.append(time_combined)
-                        
-                        print(f"Appended data for product_flag {product_flag_key}")
+                        data_vectors[f'tank{tank_num}']['press_time'].append(time_combined)
+                        data_vectors[f'tank{tank_num}']['tank_press'].append(last_pressure_values[tank_num]['tank_press'])
+                        data_vectors[f'tank{tank_num}']['reg_press'].append(last_pressure_values[tank_num]['reg_press'])
+
+                        print(f"Updated pressure values for tank {tank_num}: {last_pressure_values[tank_num]}")
+                    elif product_flag_key == 'Temperatures':  # Check if this is a temperature line
+                        if last_pressure_values[tank_num] is not None:
+                            skin_temp = float(columns[data_indices[0]])
+                            adap_temp = float(columns[data_indices[1]])
+                                
+                            # Use the last seen pressure values
+                            tank_press = last_pressure_values[tank_num]['tank_press']
+                            reg_press = last_pressure_values[tank_num]['reg_press']
+                                
+                            if reference_time_seconds is None:
+                                reference_time_seconds = float(columns[time_s_index])
+                                
+                            time_ms = float(columns[time_ms_index]) / 1000000  # Convert microseconds to seconds
+                            time_seconds = float(columns[time_s_index]) - reference_time_seconds
+                            time_combined = time_seconds + time_ms  # Combine seconds and milliseconds
+
+                            # Append data
+                            data_vectors[f'tank{tank_num}']['temp_time'].append(time_combined)
+                            data_vectors[f'tank{tank_num}']['skin_temp'].append(skin_temp)
+                            data_vectors[f'tank{tank_num}']['adap_temp'].append(adap_temp)
+                            data_vectors[f'tank{tank_num}']['tank_press'].append(tank_press)
+                            data_vectors[f'tank{tank_num}']['reg_press'].append(reg_press)
+                            time_vector.append(time_combined)
+
+                            # Debugging prints
+                            print(f"Appended time_combined: {time_combined}")
+                            print(f"Appended data for tank {tank_num}: Temp={skin_temp}, Adap Temp={adap_temp}, Tank Press={tank_press}, Reg Press={reg_press}, Time={time_combined}")
 
             elif marker in line:
                 marker_found=True
-                print("Marker found in file")
+                print(f"Marker found: {line}")
 
-# Defining Van der Waals modified equation for tanks
-def fTNK(n, R, P, T,a, b, v):
-    return (-a*b/(v**2))*n**3 + (a/v)*n**2 - (P*b + R*T)*n + P*v
+# # Defining Van der Waals modified equation for tanks
+# def fTNK(n, R, P, T,a, b, v):
+#     return (-a*b/(v**2))*n**3 + (a/v)*n**2 - (P*b + R*T)*n + P*v
 
-# Defining the derivative of Van der Waals modified equation for tanks
-def f_primeTNK(n, R, P, T,a, b, v):
-    return (-3*a*b/(v**2))*n**2 + (2*a/v)*n - (P*b) - (R*T)
+# # Defining the derivative of Van der Waals modified equation for tanks
+# def f_primeTNK(n, R, P, T,a, b, v):
+#     return (-3*a*b/(v**2))*n**2 + (2*a/v)*n - (P*b) - (R*T)
 
-# Newton-Raphson method implementation
-def newton_raphson(f, f_prime, x0, P, T, tolerance=1e-8, max_iterations=100):
-    x = x0
-    for i in range(max_iterations):
-        fx = f(x, P, T)
-        fpx = f_prime(x, P, T)
-        x_new = x - fx / fpx
-        if abs(x_new - x) < tolerance:
-            return x_new
-        x = x_new
+# # Newton-Raphson method implementation
+# def newton_raphson(f, f_prime, x0, P, T, tolerance=1e-8, max_iterations=100):
+#     x = x0
+#     for i in range(max_iterations):
+#         fx = f(x, P, T)
+#         fpx = f_prime(x, P, T)
+#         x_new = x - fx / fpx
+#         if abs(x_new - x) < tolerance:
+#             return x_new
+#         x = x_new
     
 
 def extract_date_from_filename(filename):
@@ -195,115 +209,72 @@ def perform_fft(data_vectors, time_vector):
 
 
 
-def analyze_files(vndrwl_files, fuel_est_files, marker, product_flag_index, data_indices_vndrwl, data_indices_fuel, time_s_index, time_ms_index):
+def analyze_files(vndrwl_files, marker, product_flag_index, data_indices, tank_num_index, time_s_index, time_ms_index):
     
     #initialize nested data structure and this function defininition will populate the data structure as the files are read
     nested_data={}
+    time_vector=[]
+    for filename in vndrwl_files:
+        satellite_type = extract_satellite_type_from_filename(filename)
+        if satellite_type not in nested_data:
+            nested_data[satellite_type] = {'tank1': {}, 'tank2': {}}
 
-    def process_vndrwl_file(filename):
-
-        date=extract_date_from_filename(filename)
-        print('Date Extracted: {date}')
-
-        satellite_type=extract_satellite_type_from_filename(filename)
-        print('Satellite Marker Extracted: {satellite_type}')
-
-        if date not in nested_data[date]:
-            nested_data[date]={}
-        
-        if satellite_type not in nested_data[date]:
-            nested_data[date][satellite_type]= {
-                'Temperature and Pressure': [],
-                'Fuel Mass Estimates': []
+        data_vectors = {
+            'tank1': {
+                'skin_temp': [],
+                'adap_temp': [],
+                'tank_press': [],
+                'reg_press': [],
+                'temp_time':[],
+                'press_time':[]
+            },
+            'tank2': {
+                'skin_temp': [],
+                'adap_temp': [],
+                'tank_press': [],
+                'reg_press': [],
+                'temp_time':[],
+                'press_time':[]
             }
-            
-        #Initialize lists for data storage
-        data_vectors = {                            
-            'skin_temp': [],    #Zenith direction skin temperature (Celsius)
-            'adap_temp': [],    #Nadir direction skin temperature (Celsius)
-            'tank_press':[],    #Actual tank pressure in the high CGPS pressure part (Bar)
-            'reg_press': [],    #Actual tank pressure in the low CGPS pressure part (Bar)
-            'avg_temp':  [],    #Averages of Skin and Adap
-            'avg_press': [],    #Averages of Tank and Reg
-            'vndrwl_mass_est': [], #Van der Waals estimate of gas in tank
-            'fuel_est':  []     #Fuel Mass estimates from other file structure
-            }
-        
-        # Initialize empty time vector, should be the same for every product
-        time_vector=[]
+        }
 
-        #Rewrite the function to specifically process the Van-Der-Waals type file
-        process_file_past_header(filename, marker, product_flag_index, data_indices_vndrwl, time_s_index, time_ms_index, data_vectors, time_vector)
-
-        #Average the Pressure and Temperature Readings
-        avg_press= np.mean(data_vectors['reg_press'])
-        avg_temp= np.mean(data_vectors['skin_temp'])
-
-        #Define Variables for Van Der Waals Calculation
-        # Variable initiation to compute mass differences
-        v = 52      # volume of fuel tanks in liters
-        a = 1.370   # constant for N2 (bar L^2/ mol^2)
-        b = 0.0387  # constant for N2 (L/mol)
-        R = 0.08314 # ideal gas constant in (bar L/ mol K)
-        n0 = 0.5     # initial guess for newton raphson
-        molar_mass_N2 = 28.006148008 #molar mass of N2 in g/mol
-        total_mass = 16       # 16 kg of mass per fuel tank 
-
-
-        #Append the processed data
-        nested_data[date][satellite_type]['Temperature and Pressure'].append({
-            'filename': filename,
-            data_vectors:{
-                'temperature': data_vectors[('skin_temp'+'adap_temp')/2],
-                'pressure': data_vectors[('(reg_press'+'tank_press')/2]
-            }
-        })
-        
-    #Function for Processing the Fuel Mass Estimate Files
-    def process_fuel_est_file(filename):
-        date=extract_date_from_filename(filename)
-        satellite_type= extract_satellite_type_from_filename(filename)
-
-        if date not in nested_data:
-            nested_data[date]={}
-
-        if satellite_type not in nested_data[date]:
-            nested_data[date][satellite_type]={
-                    "Temperature and Pressure": [],
-                    'Fuel Mass Estimates': []
-                }
-            
-        #Initialize list for fuel mass estimates
-        fuel_mass_estimates = []
-
-        #Process the fuel mass estimates file
-        with open(filename, 'r') as file:
-            for line in file:
-                if marker in line:
-                    continue
-                        
-                columns=line.split
-
-                if len(columns) > max(product_flag_index, *data_indices_fuel, time_s_index, time_ms_index):
-                    fuel_mass_est = float(columns[data_indices_fuel[0]])
-                    nested_data[date][satellite_type]['Fuel Mass Estimates'].append(fuel_mass_est)
-            
-    for vndrwl_file in vndrwl_files:
-        process_vndrwl_file(vndrwl_file)
-    for fuel_est_file in fuel_est_files:
-        process_fuel_est_file(fuel_est_file)
-
-
+    #Call the Function to Process the Data
+        process_file_past_header(filename, marker, product_flag_index, data_indices, tank_num_index, time_s_index, time_ms_index, data_vectors, time_vector)
+        nested_data[satellite_type] = data_vectors
     return nested_data
+
+def plot_data(data_vectors):
+    for tank, data in data_vectors.items():
+        if len(data['temp_time']) > 0:
+            plt.figure(figsize=(12, 6))
+            plt.subplot(2, 1, 1)
+            plt.plot(data['temp_time'], data['skin_temp'], label=f'Skin Temperature for {tank}')
+            plt.plot(data['temp_time'], data['adap_temp'], label=f'Adap Temperature for {tank}')
+            plt.xlabel('Time (seconds)')
+            plt.ylabel('Temperature (Celsius)')
+            plt.title(f'Temperature Data for {tank}')
+            plt.legend()
+
+        if len(data['press_time']) > 0:
+            plt.subplot(2, 1, 2)
+            plt.plot(data['press_time'], data['tank_press'], label=f'Tank Pressure for {tank}')
+            plt.plot(data['press_time'], data['reg_press'], label=f'Reg Pressure for {tank}')
+            plt.xlabel('Time (seconds)')
+            plt.ylabel('Pressure (Bar)')
+            plt.title(f'Pressure Data for {tank}')
+            plt.legend()
+
+        plt.tight_layout()
+        plt.show()
 
 
 #Define Variables for the Data Bypass
 vndrwl_files=glob.glob(r'C:\data\Fuel Leak Data\TNK1A_*-*-*_*_04.txt') #Use glob to get all files that contain the data for Van-Der-Waals Calculations
-fuel_est_files=glob.glob(r'C:\data\Fuel Leak Data\MAS1A_*-*-*_*_04.txt') #use glob to get all files that contain the fuel mass estimates the tanks of both twin stellitss per hour
+# fuel_est_files=glob.glob(r'C:\data\Fuel Leak Data\MAS1A_*-*-*_*_04.txt') #use glob to get all files that contain the fuel mass estimates the tanks of both twin stellitss per hour
 marker='# End of YAML header'
 product_flag_index= 6 
 data_indices_vndrwl= [7,8,9,10]                       # Adjust this index if the product flag is in a different column
-data_indices_fuel= [7,8]
+tank_num_index=4
 time_s_index= 0                         
 time_ms_index= 1
 
@@ -311,6 +282,8 @@ time_ms_index= 1
 time_vector = []        # Initialize empty time vector, should be the same for every product
 
 # Analyze the files
-nested_data = analyze_files(vndrwl_files, fuel_est_files, marker, product_flag_index, data_indices_vndrwl, data_indices_fuel, time_s_index, time_ms_index)
+nested_data = analyze_files(vndrwl_files, marker, product_flag_index, data_indices_vndrwl, tank_num_index, time_s_index, time_ms_index)
 
 # fft_results = perform_fft(data_vectors, time_vector)
+
+plot_data(nested_data, time_vector)
